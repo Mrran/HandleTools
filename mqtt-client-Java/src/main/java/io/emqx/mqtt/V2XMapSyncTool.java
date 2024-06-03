e7421e85917ad4f7b4d925855314015521bfbb9e
Mrran <574342154@qq.com>
2024-06-03 10:53:04 +0800

    更新

--- mqtt-client-Java/src/main/java/io/emqx/mqtt/V2XMapSyncTool.java
+++ mqtt-client-Java/src/main/java/io/emqx/mqtt/V2XMapSyncTool.java
@@ -1,12 +1,17 @@
 package io.emqx.mqtt;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SQLiteExample2 {
+public class V2XMapSyncTool {
     public static void main(String[] args) {
         // SQLite数据库连接字符串，确保指向正确的数据库文件
         String dbURL = "jdbc:sqlite:D:\\work\\doc\\business\\rtty\\V2X-MapMark-master\\V2X-MapMark-master\\MapMark\\MapCollect.db";
-        String[] inputArray = {
+        /*String[] inputArray = {
                 "203-204", "185-186"
                 //"193-192",
                 //"192-193",
@@ -15,13 +20,29 @@ public class SQLiteExample2 {
                 //"395-305","305-395","3402-305","305-3402","305-3403","3403-305","305-198","198-305","304-198","198-304","198-199","199-198","199-381","381-383","383-199","199-200","200-199","200-428","428-200",
                 //"200-201","201-200","201-429","429-201","201-202","236-201","202-186","186-236","235-236","202-203","186-187","187-186","301-187","186-306","203-306","300-186","187-239","239-187","187-434","189-187",
                 //"434-436","435-436","436-189","185-235","186-204","184-185","204-184","184-238","238-184","184-206","182-184","184-234","234-184"
-        };
+        };*/
+
+
+        String filePath = "D:\\work\\doc\\business\\rtty\\V2X-MapMark-master\\new_lane_import.txt";
+        List<String> inputList = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                inputList.add(line.trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 转换List为Array
+        String[] inputArray = inputList.toArray(new String[0]);
 
         try (Connection conn = DriverManager.getConnection(dbURL)) {
             if (conn != null) {
 
                 // 第二个数据库的文件路径，需要附加到主数据库
-                String secondDbPath = "D:\\work\\doc\\business\\rtty\\V2X-MapMark-master\\V2X-MapMark-master\\MapMark\\MapCollect_new_collect.db";
+                String secondDbPath = "D:\\work\\doc\\business\\rtty\\V2X-MapMark-master\\杨修正后的地图\\20240523\\MapCollect_new2(10).db";
 
                      Statement stmt = conn.createStatement();
 
